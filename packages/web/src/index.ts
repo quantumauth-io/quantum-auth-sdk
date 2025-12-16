b7181618dba9f0b016f4621950010d5223e15e09
madeindreams <madeindreams@protonmail.com>
2025-12-15 16:40:56 -0800

    Update index.ts

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -87,12 +87,12 @@ export class QuantumAuthWebClient {
             );
         }
 // todo move this outside of the request.
-        // const hasExtension = await isQuantumAuthExtensionAvailable();
-        // if (!hasExtension) {
-        //     throw new Error(
-        //         "QuantumAuth browser extension not detected. Please install the QuantumAuth extension to use protected requests.",
-        //     );
-        // }
+        const hasExtension = await isQuantumAuthExtensionAvailable();
+        if (!hasExtension) {
+            throw new Error(
+                "QuantumAuth browser extension not detected. Please install the QuantumAuth extension to use protected requests.",
+            );
+        }
 
         const resp = await qaRequest<{
             qaProof?: Record<string, string>;
